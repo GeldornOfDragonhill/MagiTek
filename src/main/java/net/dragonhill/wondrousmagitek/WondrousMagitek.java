190ab0002a0d3090890da2cac5266fab8fbefb00
Stefan Klinger <geldorn@dragonhill.net>
2020-05-11 01:44:50 +0200

    Basic structure.

---
+++ src/main/java/net/dragonhill/wondrousmagitek/WondrousMagitek.java
@@ -0,0 +1,9 @@
+package net.dragonhill.wondrousmagitek;
+
+import net.dragonhill.wondrousmagitek.config.Constants;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(Constants.modId)
+public class WondrousMagitek {
+
+}
---
+++ src/main/java/net/dragonhill/wondrousmagitek/config/Constants.java
@@ -0,0 +1,5 @@
+package net.dragonhill.wondrousmagitek.config;
+
+public final class Constants {
+    public static final String modId = "wondrousmagitek";
+}
