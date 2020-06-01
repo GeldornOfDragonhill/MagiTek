102d4ce16aaf7c0e26cd35b48fb1835080b60cf9
Mario Fraiss <mario@fraiss.at>
2020-06-01 11:56:40 +0200

    Implemented texture facing of AreaStabilizerBlock, Designed and added new textures of AreaStabilizerBlock and TickBlock

--- src/main/java/net/dragonhill/wondrousmagitek/blocks/areastabilizer/AreaStabilizerBlock.java
+++ src/main/java/net/dragonhill/wondrousmagitek/blocks/areastabilizer/AreaStabilizerBlock.java
@@ -3,6 +3,7 @@ package net.dragonhill.wondrousmagitek.blocks.areastabilizer;
 import net.dragonhill.wondrousmagitek.blocks.BlockBase;
 import net.dragonhill.wondrousmagitek.config.Config;
 import net.dragonhill.wondrousmagitek.init.ModTileEntities;
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.material.Material;
 import net.minecraft.entity.LivingEntity;
@@ -10,8 +11,11 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.inventory.container.INamedContainerProvider;
 import net.minecraft.item.ItemStack;
+import net.minecraft.state.StateContainer;
+import net.minecraft.state.properties.BlockStateProperties;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.ActionResultType;
+import net.minecraft.util.Direction;
 import net.minecraft.util.Hand;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.BlockRayTraceResult;
@@ -43,6 +47,11 @@ public class AreaStabilizerBlock extends BlockBase {
 
 	@Override
 	public void onBlockPlacedBy(World worldIn, BlockPos pos, BlockState state, @Nullable LivingEntity placer, ItemStack stack) {
+
+		if (placer != null) {
+			worldIn.setBlockState(pos, state.with(BlockStateProperties.FACING, getFacingFromEntity(pos, placer)), 2);
+		}
+
 		if(worldIn.isRemote) {
 			return;
 		}
@@ -62,5 +71,12 @@ public class AreaStabilizerBlock extends BlockBase {
 		return ModTileEntities.areaStabilizerTileEntity.get().create();
 	}
 
+	@Override
+	protected void fillStateContainer(StateContainer.Builder<Block, BlockState> builder) {
+		builder.add(BlockStateProperties.FACING);
+	}
 
+	public static Direction getFacingFromEntity(BlockPos clickedBlock, LivingEntity entity) {
+		return Direction.getFacingFromVector((float) (entity.getPosX() - clickedBlock.getX()), (float) (entity.getPosY() - clickedBlock.getY()), (float) (entity.getPosZ() - clickedBlock.getZ()));
+	}
 }
